72e3b634df290848338fc009322f867c7a344e45
zhiyongzaixian <13852292775@163.com>
2019-12-12 10:19:45 +0800

    'first'

--- src/main.js
+++ src/main.js
@@ -3,6 +3,21 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+
+
+// new Vue({
+//   el: '#app',
+//   render: h => h(App),
+// })
+
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  components: {App},
+  template: '<App />'
+})
+
+console.log('xxx');
---
+++ vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  runtimeCompiler: true,
+  lintOnSave: false, // 关闭EsLint的规则
+}
